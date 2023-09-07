e8671450029be2c4a8b2983875160d08ec7a6a14
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-09-07 16:00:41 +0400

    complete layout for consultation tab --feature

--- src/widgets/profile/tabs/tabs/consultation/index.tsx
+++ src/widgets/profile/tabs/tabs/consultation/index.tsx
@@ -1,6 +1,7 @@
-import React, { Fragment } from "react";
+import React from "react";
 import style from './Style.module.scss';
 import { Camera } from "../../../../../shared/core/ui/svg/camera";
+import { Friends } from "../../../../../shared/core/ui/svg/friends";
 
 export const ProfileTabConsultation = () => {
 
@@ -29,6 +30,21 @@ export const ProfileTabConsultation = () => {
                     <p className={style['ConsultationTime']}>15.01.2019 12:30-13:00</p>
                 </div>
             </div>
+        </div>
+
+        <div className={style['Consultation']}>
+            <div className={style['ConsultationContent']}>
+                <div className={style['ConsultationCamera']}>
+                    <Friends />
+                </div>
+
+                <div className={style['ConsultationDescription']}>
+                    <p className={style['ConsultationTitle']}>Личный приём</p>
+                    <p className={style['ConsultationTime']}>15.01.2019 12:30-13:00</p>
+                </div>
+            </div>
+
+            <p className={style['ConsultationNotConfirmed']}>Не подтверждена</p>
         </div>
     </div>
 }
